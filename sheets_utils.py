2b28aefa7896a5d975f4ccf9584b2d150d1a60f2
DonBo13127 <73437211+DonBo13127@users.noreply.github.com>
2025-09-20 13:46:00 +0200

    Update sheets_utils.py

--- sheets_utils.py
+++ sheets_utils.py
@@ -5,16 +5,9 @@ def connect_sheets(json_keyfile, sheet_url):
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile, scope)
     client = gspread.authorize(creds)
-    try:
-        sheet = client.open_by_url(sheet_url).sheet1
-        return sheet
-    except Exception as e:
-        raise Exception(f"⚠️ Impossible de se connecter à Google Sheet : {e}")
+    return client.open_by_url(sheet_url).sheet1
 
 def save_job(sheet, job):
-    """
-    Enregistre l'offre dans Google Sheets si elle n'existe pas déjà
-    """
     existing = sheet.col_values(1)
     if job['title'] not in existing:
         sheet.append_row([
